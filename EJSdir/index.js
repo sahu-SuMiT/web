fde5fc27bb6b5adc51c7131a80350cfe44337533
SuMiT <145762361+sahu-SuMiT@users.noreply.github.com>
2025-01-15 22:53:55 +0530

    Add files via upload

---
+++ EJSdir/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.set('views',path.join(__dirname,'/views'));
+const port = 8080;
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+app.get('/hello',(req,res)=>{
+    res.send('hello');
+});
+app.get('/ig/:username',(req,res)=>{
+    let {username} = req.params;
+    const instaData = require('./data.json');
+    const data = instaData[username];
+    if(data){
+        res.render('instagram.ejs',{data} );
+    }
+    else{
+        res.render('error.ejs');
+    }
+
+
+    
+});
+app.get('/rolldice',(req,res)=>{
+    let num = Math.floor(Math.random()*6 + 1);
+    res.render('rolldice.ejs',{diceVal:num});
+});
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+});
